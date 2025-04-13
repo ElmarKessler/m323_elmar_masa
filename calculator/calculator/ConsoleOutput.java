8a8093eada9dfd85c5f968964cad2e370df83953
Generaladvisor-Lam <sheung-tsung.lam@noseryoung.com>
2025-04-14 00:20:18 +0200

    Rewritten user input logic

--- calculator/calculator/ConsoleOutput.java
+++ calculator/calculator/ConsoleOutput.java
@@ -4,7 +4,7 @@ class ConsoleOutput {
     public static void print(String message) {
         System.out.println(message);
     }
-    
+
     public static void printResult(String operation, double result) {
         System.out.printf("%s = %.2f%n", operation, result);
     }
